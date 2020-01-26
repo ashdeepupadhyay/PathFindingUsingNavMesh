d3e7b20906f17ea4d22ebfdb10a65cea5b1af879
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2020-01-26 21:08:37 +0530

    Path Finding using Navmesh

---
+++ navigationAINavMesh/Assets/Scripts/FindPath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FindPath : MonoBehaviour
+{
+    public GameObject destination;
+    NavMeshAgent navMeshAgent;
+
+    // Use this for initialization
+    void Start()
+    {
+        //destination = GameObject.Find("Destination");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(destination.transform.position);
+
+    }
+
+}
